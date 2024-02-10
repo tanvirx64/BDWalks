f604ff888fdb468ea58b185a2398f01ceb618ade
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-02-10 20:19:59 +0600

    Region and Difficulty Seeder Added

--- BDWalks.API/Migrations/20240210141033_Seeding Data for Difficulties and Regions.Designer.cs
+++ BDWalks.API/Migrations/20240210141033_Seeding Data for Difficulties and Regions.Designer.cs
@@ -4,6 +4,7 @@ using BDWalks.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BDWalks.API.Migrations
 {
     [DbContext(typeof(BDWalksDbContext))]
-    partial class BDWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240210141033_Seeding Data for Difficulties and Regions")]
+    partial class SeedingDataforDifficultiesandRegions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
